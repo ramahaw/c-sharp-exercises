6128fa9b95ab8ac01c0b94ac57b2b0ea5e5caab8
ramahaw <ramarore41@gmail.com>
2018-11-07 00:14:04 +0200

    Add MaxNumber.cs

---
+++ MaxNumber.cs
@@ -0,0 +1,38 @@
+﻿// Max Number
+// First, tell me how many numbers you want to enter.‏
+// Then, enter the numbers.
+// After that, the program will print the biggest number.
+‏
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison
+{
+    class MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers?");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number: ");
+            int maxNum = Convert.ToInt32(Console.ReadLine());
+
+            while (--n > 0)
+            {
+                Console.Write("Enter number: ");
+                int tempNum = Convert.ToInt32(Console.ReadLine());
+                if (tempNum > maxNum)
+                {
+                    maxNum = tempNum;
+                }
+            }
+
+            Console.WriteLine("The biggest number is: " + maxNum);
+            Console.ReadKey();
+        }
+    }
+}
