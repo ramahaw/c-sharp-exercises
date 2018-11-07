ca842d54591e6f7feacdc5e2cf55cf90aa47c50b
ramahaw <ramarore41@gmail.com>
2018-11-08 00:14:46 +0200

    add multipliction

---
+++ Multipliction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication
+{
+    class Multipliction
+    {
+        static void Main(string[] args)
+        {
+            //this program give you the multipliction table of any number
+
+            Console.WriteLine("Please enter number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10 ; i++)
+            {
+                int result = x * i;
+                Console.WriteLine(i+ "*" +x+ "=" + result);
+            }
+        }
+    }
+}
