f075b2b561a17e041a389748fa5641166294df6c
ramahaw <44590755+ramahaw@users.noreply.github.com>
2018-11-08 00:59:34 +0200

    Add multiplication (#3)
    * add multipliction
    
    * code enhancement

---
+++ Multiplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication
+{
+    class Multiplication
+    {
+        static void Main(string[] args)
+        {
+            // This program will give you the multiplication table of any number you enter
+
+            Console.WriteLine("Please enter a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = x * i;
+                Console.WriteLine(i + " * " + x + " = " + result);
+            }
+        }
+    }
+}
