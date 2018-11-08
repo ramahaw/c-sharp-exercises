3c2e847271d514abd02b0ca10afd5cc45dcb6efd
ramahaw <ramarore41@gmail.com>
2018-11-08 22:18:44 +0200

    Add EvenOrOdd

---
+++ EvenOrOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            while (x > 1)
+            {
+                x = x - 2;
+            }
+            if (x == 0)
+                Console.WriteLine("even");
+            else
+                Console.WriteLine("odd");
+        }
+    }
+}
