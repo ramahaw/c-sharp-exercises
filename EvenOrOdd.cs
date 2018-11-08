8ae90c70b67636b91abda538541dbba0138e0f35
ramahaw <44590755+ramahaw@users.noreply.github.com>
2018-11-09 00:18:57 +0200

    Add EvenOrOdd (#7)
    * Add EvenOrOdd
    
    * Code enhancements

---
+++ EvenOrOdd.cs
@@ -0,0 +1,33 @@
+﻿
+// ‏Find out if the entered number is odd or even by using -2‏
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (number > 1)
+            {
+                number -= 2;
+            }
+
+            if (number == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
