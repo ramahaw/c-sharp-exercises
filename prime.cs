97d7955fff9c99030054b3ac31ec4bcff006f8a5
ramahaw <ramarore41@gmail.com>
2018-11-08 21:26:45 +0200

    Add Prime

---
+++ prime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class prime
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            while (i < x)
+            {
+                if (x % i == 0)
+                {
+                    Console.WriteLine("not prime");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    if (x-i==1)
+                    {
+                        Console.WriteLine("prime");
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
