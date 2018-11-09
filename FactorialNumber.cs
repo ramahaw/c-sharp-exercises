9b3ccd78c167b9c7f772d7958f068d4cf1fc09d8
ramahaw <ramarore41@gmail.com>
2018-11-09 21:43:05 +0200

    Add FactorialNumber

---
+++ FactorialNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumber
+{
+    class FactorialNumber
+    {
+        static void Main(string[] args)
+        {
+            int y = 1;
+            Console.Write("x=  ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= x; i++)
+            {
+                Console.WriteLine(i);
+                y = i * y;
+            }
+            Console.WriteLine("x! =" + y);
+        }
+    }
+}
