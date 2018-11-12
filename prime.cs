23fdffe14c08d637d476d3cf419abb9e00106c65
ramahaw <ramarore41@gmail.com>
2018-11-12 22:55:22 +0200

    code enhancement

--- prime.cs
+++ prime.cs
@@ -1,26 +1,21 @@
-﻿
-//find out if the entered number is primary or not primary. 
+﻿using System;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace prime
+namespace Prime
 {
     class Prime
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number to know if it's primary or not");
             int number = Convert.ToInt32(Console.ReadLine());
-            int i = 2;
+
             if (number == 1 || number == 0)
             {
                 Console.WriteLine("Primary");
             }
             else
             {
+                int i = 2;
                 while (i < number)
                 {
                     if (number % i == 0)
@@ -30,14 +25,13 @@ namespace prime
                     }
                     else
                     {
-                        i++;
                         if (number - i == 1)
                         {
                             Console.WriteLine("Primary");
                         }
                     }
+                    i++;
                 }
-
             }
             Console.ReadKey();
         }
