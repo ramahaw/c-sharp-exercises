480290f52495e61b5261d1677918fb4da3d9ae33
ramahaw <44590755+ramahaw@users.noreply.github.com>
2018-11-12 22:57:40 +0200

    Add Prime (#4)
    * Add Prime
    
    * enhancement prim
    
    * code enhancement

---
+++ prime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Prime
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to know if it's primary or not");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 1 || number == 0)
+            {
+                Console.WriteLine("Primary");
+            }
+            else
+            {
+                int i = 2;
+                while (i < number)
+                {
+                    if (number % i == 0)
+                    {
+                        Console.WriteLine("Not Primary");
+                        break;
+                    }
+                    else
+                    {
+                        if (number - i == 1)
+                        {
+                            Console.WriteLine("Primary");
+                        }
+                    }
+                    i++;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
