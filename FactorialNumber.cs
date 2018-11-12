2c7f65a021909a10bfd3401991e10a7c8599f08a
ramahaw <44590755+ramahaw@users.noreply.github.com>
2018-11-12 22:22:44 +0200

    Add Factorial Number (#8)
    * Add FactorialNumber
    
    * Add  Factorial Number enhacement

---
+++ FactorialNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumber
+{
+    class FactorialNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a positve number to get its factorial");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+            for (int i = number; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(number + "!= " + result);
+            Console.ReadKey();
+        }
+    }
+}
