d00d63d3ec56936ced68005389b182242c2ac546
ramahaw <44590755+ramahaw@users.noreply.github.com>
2018-11-15 23:53:06 +0200

    Add PowerNumber (#10)
    * Add PowerNumber
    
    * Enhancement PoweNumber

---
+++ PowerNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PowerNumber
+{
+    class PowerNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a power:");
+            int power = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+            while (power > 0)
+            {
+                result *= number;
+                power--;
+            }
+            Console.WriteLine("The result is:" + result);
+            Console.ReadKey();
+        }
+    }
+}
