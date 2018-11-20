0c962a2f459380a373edc8831e20fb483c94bd1e
ramahaw <ramarore41@gmail.com>
2018-11-20 20:05:54 +0200

    Add Triangle

---
+++ Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+
+{
+    class Triangle
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number of lines:");
+            int line = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < line; i++)
+            {
+                for (int stars = 0; stars <= i; stars++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
+
